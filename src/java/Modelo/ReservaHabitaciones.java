465ff5c3b1b0a4eec257938cc8fa2fc951bc7c4e
StayWellProduction <UnTalJuan2006>
2025-12-02 07:59:14 -0500

    Desarrollo Habitaciones multiples

---
+++ src/java/Modelo/ReservaHabitaciones.java
@@ -0,0 +1,74 @@
+package Modelo;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class ReservaHabitaciones implements Serializable {
+
+   
+    private int id;
+    private Reserva reserva;
+    private Habitacion habitacion;
+    private EnumEstadoReservaHabitacion estado;
+    private String observaciones;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaActualizacion;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public EnumEstadoReservaHabitacion getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EnumEstadoReservaHabitacion estado) {
+        this.estado = estado;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDateTime getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(LocalDateTime fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+}
